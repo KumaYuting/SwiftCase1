1b374418345115e169af333119bc67f89ec8b355
Yuting.jheng <Yuting.Jheng@acti.com>
2024-11-14 20:13:44 +0800

    Initial Commit

---
+++ SwiftCase1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftCase1
+//
+//  Created by 雨停 on 2024/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftCase1Tests/SwiftCase1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCase1Tests.swift
+//  SwiftCase1Tests
+//
+//  Created by 雨停 on 2024/11/14.
+//
+
+import Testing
+@testable import SwiftCase1
+
+struct SwiftCase1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
