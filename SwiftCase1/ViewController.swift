18da85c6f632ad00c008123b50cc6cdf34d4d542
Yuting.jheng <Yuting.Jheng@acti.com>
2024-11-15 23:57:09 +0800

    v1.01 Update ViewController

--- SwiftCase1/ViewController.swift
+++ SwiftCase1/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var loginBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loginBtn.titleLabel?.font = UIFont(name: "QTxiaotu", size: 66)
     }
 
 
