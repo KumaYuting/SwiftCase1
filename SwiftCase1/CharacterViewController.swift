27f1209d7875c270d6a750aab8a03626d63315a2
Yuting.jheng <Yuting.Jheng@acti.com>
2024-11-16 17:35:23 +0800

    v1.01 Add View Controller to storyboard.

---
+++ SwiftCase1/CharacterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CharacterViewController.swift
+//  SwiftCase1
+//
+//  Created by 雨停 on 2024/11/16.
+//
+
+import UIKit
+
+
+class CharacterViewController: UIViewController {
+    @IBOutlet weak var characterLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        characterLabel.font = UIFont(name: "ARTianniuB5-Bold", size: 30)
+
+    }
+}
+
